92062d08a08b67281dfe254b3d20659bcf854614
TheoPaillusson <70954633+TheoPaillusson@users.noreply.github.com>
2020-11-23 16:13:44 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 with open('requirements.txt') as f:
     content = f.readlines()
-requirements = [x.strip() for x in content if 'git+' not in x]
+requirements = install_requires=requirements, [x.strip() for x in content if 'git+' not in x]
 
 setup(name='sharetools',
       version="1.0",
